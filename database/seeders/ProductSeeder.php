ef198fa72c4a4ef0480a2ba05c1175a32c15bb8b
Jordi <jordi.tomas@studiogenesis.es>
2024-06-11 09:14:54 +0200

    Add new seeders and other updates

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+// database/seeders/ProductSeeder.php
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::factory()->count(20)->create();
+    }
+}
+
